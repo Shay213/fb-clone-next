28e7fd508d090601600770b811183fddb0973ebd
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-06-01 17:39:19 +0200

    Add initial add post ui

--- app/(home)/components/addPost/AddPost.tsx
+++ app/(home)/components/addPost/AddPost.tsx
@@ -1,7 +1,66 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
+import { AiFillVideoCamera } from "react-icons/ai";
+import { BsImages } from "react-icons/bs";
+import { CiFaceSmile } from "react-icons/ci";
+
 const AddPost = () => {
-  return <div>AddPost</div>;
+  const { data: session } = useSession();
+  const name = session?.user?.name?.split(" ")[0];
+
+  return (
+    <div className="mt-4 bg-white rounded-md shadow-lg py-2 px-6">
+      <div className="flex items-center gap-3">
+        <Image
+          src={session?.user?.image || "/avatar.jpeg"}
+          alt="user"
+          width={35}
+          height={35}
+        />
+        <button
+          type="button"
+          className="
+              px-4 py-2 bg-gray-100 text-gray-600 rounded-full 
+              flex-1 text-sm text-left hover:bg-gray-200 transition
+            "
+        >{`What's on your mind, ${name}`}</button>
+      </div>
+      <hr className="my-2" />
+      <div className="flex">
+        <div
+          className="
+            flex items-center justify-center gap-2 flex-1 py-2
+            hover:bg-gray-200 transition cursor-pointer rounded-md
+          "
+        >
+          <AiFillVideoCamera className="fill-red-500" size={25} />
+          <span className="text-gray-600 text-sm">Live video</span>
+        </div>
+        <div
+          className="
+            flex items-center justify-center gap-2 flex-1 py-2
+            hover:bg-gray-200 transition cursor-pointer rounded-md
+          "
+        >
+          <BsImages className="fill-green-500" size={25} />
+          <span className="text-gray-600 text-sm">Photo/video</span>
+        </div>
+        <div
+          className="
+            flex items-center justify-center gap-2 flex-1 py-2
+            hover:bg-gray-200 transition cursor-pointer rounded-md
+          "
+        >
+          <CiFaceSmile className="fill-amber-500" size={25} />
+          <span className="text-gray-600 text-sm">Feeling/activity</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
