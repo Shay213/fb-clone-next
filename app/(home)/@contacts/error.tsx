f61ab2540c247ddcdb967b069af4a210bae71979
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-06-02 15:19:42 +0200

    Add error pages to feed and contacts

---
+++ app/(home)/@contacts/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  useEffect(() => {
+    //Log the error to an error reporting service
+    console.log(error);
+  }, [error]);
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button type="button" onClick={() => reset()}>
+        Try again
+      </button>
+    </div>
+  );
+};
+
+export default Error;
